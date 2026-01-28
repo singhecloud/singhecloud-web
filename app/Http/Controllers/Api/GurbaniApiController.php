3d8636dc76b1fa6e51e1038dc60a8c5e62a12590
Sukhpal Singh <sukhpalmail@protonmail.com>
2026-01-28 21:49:34 +1000

    Feature: extending api to 150 angs

--- app/Http/Controllers/Api/GurbaniApiController.php
+++ app/Http/Controllers/Api/GurbaniApiController.php
@@ -9,7 +9,7 @@ class GurbaniApiController
     public function getByAng($ang)
     {
         // Ensure ang is within valid range
-        if ($ang < 1 || $ang > 120) {
+        if ($ang < 1 || $ang > 150) {
             return;
         }
 
